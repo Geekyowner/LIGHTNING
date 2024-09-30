063ce7e77079c1dfd318d28d4c7ce8a51aa95695
Ankit <155639266+gojo22k@users.noreply.github.com>
2024-10-01 00:19:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,14 +14,14 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID = os.environ.get("API_ID", "24817837")  # ⚠️ Required
-    API_HASH = os.environ.get("API_HASH", "acd9f0cc6beb08ce59383cf250052686")  # ⚠️ Required
+    API_ID = os.environ.get("API_ID", "25198711")  # ⚠️ Required
+    API_HASH = os.environ.get("API_HASH", "2a99a1375e26295626c04b4606f72752")  # ⚠️ Required
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7087125626:AAEDIbGK46wjDhpk5NCknDzB8upJtJXRZLM")  # ⚠️ Required
     
     # premium 4g renaming client
     STRING_API_ID = os.environ.get("STRING_API_ID", "")
     STRING_API_HASH = os.environ.get("STRING_API_HASH", "")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "BAClFnqpUj6rZUi-eKlefYSm66f5Yjwx-Dd-UlAiOWD2nFjB1o_i-w13f9HOQnO6JAnV822ACGCzapUquUUTL3a4-4PqCPsxnn_BbJWZBB9JSqBBZaKV8Ijmt2qw1h4MlivE-I6YlL5bQgMFZhQOMunYNzTaOevv9dJwuq1qTNBN260cBcfUCL1NYppN_6OpT_hkZyPLClkWgZGAKWZVNn9Y96sRirRUuJ6Tb5PF863t1-ENqvR8rgs1pDTc4hOkdtrbvIPcCl7mhviq-FF51ZnEciyBu57qe0A0iU9DpKWaR4KKifdenjv8_CXz9p2-zdv9PVd-UBHuC1WRd6jzXNV9AAAAAbrGtZQA")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
     # database config
     DB_URL = environ.get("DB_URL", "mongodb+srv://mhsm:mhsm@cluster0.j9figvh.mongodb.net/?retryWrites=true&w=majority")
